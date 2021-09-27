b7da2955df8ef6da000920ae62c83dde36e867fb
SamuelHFS <samuel.jpg1@gmail.com>
2021-09-27 11:33:36 -0300

    teste relative

---
+++ RelativeLayout/RelativeLayout/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RelativeLayout
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private async void T1(object sender, EventArgs e)
+        {
+            img1.Opacity = 0.5;
+            await Task.Delay(200);
+            img1.Opacity = 1;
+            await DisplayAlert("Informção", "jureg", "ok");
+        }
+    }
+}
---
+++ RelativeLayout/RelativeLayout/async.cs
@@ -0,0 +1,6 @@
+﻿namespace RelativeLayout
+{
+    internal class async
+    {
+    }
+}
